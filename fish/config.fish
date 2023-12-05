6ae21a433edf2c3e034dc35de8aca852b16039e5
Thitipong Tapianthong <akikungz@outlook.com>
2023-12-05 13:50:52 +0700

    Init repository

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+set fish_greeting ""
+
+# alias list directory contents
+alias ll "ls -l"
+alias la "ls -la"
+# alias to cd to parent directory
+alias .. "cd .."
+# alias to sync time
+alias stime "sudo ntpdate -u time.apple.com"
+
+# set color prompt
+set -g fish_color_command cyan
