2016097d970973ba2e28ad7fe15fed6272636540
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-02-05 15:33:05 +0700

    Add setup script for installing dependencies and configuring environment

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Install dependencies
+sudo apt update
+sudo apt install tmux
+
+# git submodule update --init --recursive
+git submodule update --init --recursive
+
+# Copy configuration files
+cp -a . ~/.config
+
+# global bashrc use .bashrc
+echo "source ~/.config/bash/.bashrc" >> ~/.bashrc
